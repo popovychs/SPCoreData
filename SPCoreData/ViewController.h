95c0e57f85a260958d67f76ec8387d4d690c8751
Stanislav Popovych <popovychs@gmail.com>
2015-11-16 22:49:15 +0200

    Initial Commit

---
+++ SPCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SPCoreData
+//
+//  Created by popovychs on 16.11.15.
+//  Copyright © 2015 popovychs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
